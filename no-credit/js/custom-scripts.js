7d4fb2d203e48139ac5e94faedd0fe876cb931e9
Iegor Stetsiura <biropka@gmail.com>
2016-03-01 01:17:01 +0200

    v 0.5-freedom

--- no-credit/js/custom-scripts.js
+++ no-credit/js/custom-scripts.js
@@ -1,3 +1,20 @@
-/**
- * Created by Ыгорка on 28.02.2016.
- */
+$(document).ready(function () {
+    $('.question').click(
+        function() {
+            var esAnswer = $(this).parent().find('.answer');
+            var esIconArrow = $(this).parent().find('.icon-arrow');
+            if(esAnswer.hasClass('hidden')){
+                esAnswer.removeClass('hidden');
+                esAnswer.fadeIn();
+                esIconArrow.removeClass('to-right');
+                esIconArrow.addClass('to-bottom');
+
+            } else {
+                esAnswer.addClass('hidden');
+                esAnswer.fadeOut();
+                esIconArrow.removeClass('to-bottom');
+                esIconArrow.addClass('to-right');
+            }
+
+        });
+});
